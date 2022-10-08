933e32b0ce49e64c233538236bea60b6ff4f1854
junichi <tmdjnch0901@gmail.com>
2022-10-08 11:39:16 +0900

    Todoアプリ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # 新規登録後のリダイレクト先をマイページへ
+  def after_sign_in_path_for(_resource)
+    if current_user
+      flash[:notice] = 'ログインに成功しました'
+      todos_path # 　指定したいパスに変更
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_063749) do
+ActiveRecord::Schema.define(version: 2022_10_08_015504) do
+
+  create_table "todos", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "todo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2022_10_07_063749) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "todos", "users"
 end
